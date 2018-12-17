6943b7894bd0dddb51518962894796c746b1a06f
Brendon Forsgren <bforsgren29@gmail.com>
2018-12-17 09:21:47 -0700

    Added a script to install python3 and python2

---
+++ scripts/installPython.sh
@@ -0,0 +1,19 @@
+sudo apt update && sudo apt -y upgrade
+
+sudo apt install python3-pip
+
+pip3 install numpy
+pip3 install scipy
+pip3 install matplotlib
+pip3 install pandas
+pip3 install control
+sudo apt install gfortran
+pip3 install slycot
+
+sudo apt install python-pip
+pip install numpy
+pip install scipy
+pip install matplotlib
+pip install pandas
+pip install control
+pip install slycot

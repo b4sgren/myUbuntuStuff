47dd6487b95ef2e35edc3930aa68dd654662831a
Brendon Forsgren <bforsgren29@gmail.com>
2018-12-17 10:03:32 -0700

    Added a command for the noevim stuff

--- scripts/otherTasks.sh
+++ scripts/otherTasks.sh
@@ -4,6 +4,8 @@ sudo apt install terminator
 sudo apt-add-repository ppa:neovim-ppa/stable
 sudo apt update
 sudo apt install neovim
+mkdir -p ~/.config/nvim
+echo 'source ~/.nvimrc' > ~/.config/nvim/init.vim
 
 sudo apt remove --purge libreoffice*
 sudo apt clean

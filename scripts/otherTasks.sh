d04c3fe3a6933576ada2abc6c8bc6b121f2fea71
Brendon Forsgren <bforsgren29@gmail.com>
2018-12-17 09:55:09 -0700

    Added a few other tasks to install some stuff and delete other stuff

---
+++ scripts/otherTasks.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt install terminator
+
+sudo apt-add-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install neovim
+
+sudo apt remove --purge libreoffice*
+sudo apt clean
+sudo apt autoremove

291985eb5a9b4f8ee415e1108776f7afc18e5619
Brendon Forsgren <bforsgren29@gmail.com>
2018-12-18 10:34:51 -0700

    Configured my global git name and email

--- scripts/otherTasks.sh
+++ scripts/otherTasks.sh
@@ -1,6 +1,9 @@
 sudo apt update
 
 sudo apt install git
+git config --global user.name "Brendon Forsgren"
+git config --global user.email "bforsgren29@gmail.com"
+
 sudo apt install terminator
 
 sudo apt-add-repository ppa:neovim-ppa/stable

1e4105be7a7dab6b6beb5d5d996bbcdd844590df
Brendon Forsgren <bforsgren29@gmail.com>
2019-01-07 15:29:07 -0700

    Edited the python installation. Slycot did not install correctly. Need to find out why

--- scripts/installPython.sh
+++ scripts/installPython.sh
@@ -7,6 +7,7 @@ pip3 install scipy
 pip3 install matplotlib
 pip3 install pandas
 pip3 install control
+sudo apt install python3-tk
 sudo apt install gfortran
 pip3 install slycot
 
@@ -16,4 +17,5 @@ pip install scipy
 pip install matplotlib
 pip install pandas
 pip install control
+sudo apt install python-tk
 pip install slycot

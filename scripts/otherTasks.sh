3f6a67de84cf62a9c8ff8047f35e9bb064243bda
Brendon Forsgren <bforsgren29@gmail.com>
2019-01-09 14:41:36 -0700

    Added pycharm install community edition

--- scripts/otherTasks.sh
+++ scripts/otherTasks.sh
@@ -24,3 +24,6 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo apt update
 sudo apt install spotify-client
+
+sudo apt install snap
+sudo snap install pycharm-community --classic

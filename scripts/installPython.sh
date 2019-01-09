426eb90f21d44e9037fdfa4e6d212af1974e7e04
Brendon Forsgren <bforsgren29@gmail.com>
2019-01-09 14:25:31 -0700

    Changed the python installation

--- scripts/installPython.sh
+++ scripts/installPython.sh
@@ -1,21 +1,18 @@
 sudo apt update && sudo apt -y upgrade
 
-sudo apt install python3-pip
+sudo apt install  python3-dev python3-pip python3-tk
+pip3 install --upgrade pip setuptools wheel --user
 
-pip3 install numpy
-pip3 install scipy
-pip3 install matplotlib
-pip3 install pandas
-pip3 install control
-sudo apt install python3-tk
+pip3 install numpy --user
+pip3 install scipy --user
+pip3 install matplotlib --user
+pip3 install pybind11 --user
+pip3 install pandas --user
+pip3 install control --user
+pip3 install pyqt5 --user
+pip3 install jupyter --user
+pip3 install tesserocr --user
+pip3 install pytesseract --user
+pip3 install pyopencl --user
 sudo apt install gfortran
-pip3 install slycot
-
-sudo apt install python-pip
-pip install numpy
-pip install scipy
-pip install matplotlib
-pip install pandas
-pip install control
-sudo apt install python-tk
-pip install slycot
+pip3 install slycot --user
---
+++ scripts/installPython2.sh
@@ -0,0 +1,17 @@
+sudo apt update && sudo apt -y upgrade
+
+sudo apt install  python-dev python-pip
+pip2 install --upgrade pip setuptools wheel --user
+
+pip2 install numpy --user
+pip2 install scipy --user
+pip2 install matplotlib --user
+pip2 install pybind11 --user
+pip2 install pandas --user
+pip2 install control --user
+pip2 install jupyter --user
+pip2 install tesserocr --user
+pip2 install pytesseract --user
+pip2 install pyopencl --user
+sudo apt install gfortran
+pip2 install slycot --user

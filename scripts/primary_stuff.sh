f670a665609ee25ce60da96dddba294c945f9c91
Brendon Forsgren <bforsgren29@gmail.com>
2019-01-09 14:40:23 -0700

    Added the script to install basic stuff for my installation

---
+++ scripts/primary_stuff.sh
@@ -0,0 +1,4 @@
+sudo apt install exfat-utils htop ssh mesa-utils mesa-utils-extra build-essential manpages-dev libeigen3-dev
+sudo apt install libopenblas-dev liblapacke-dev tesseract-ocr libtesseract-dev libleptonica-dev libboost-all-dev
+sudo apt install libtbb-dev ocl-icd-opencl-dev ocl-icd-libopencl1 opencl-headers clinfo libffi6 libffi-dev gcc cmake
+sudo apt install pybind11-dev gdb libtool make libcanberra-gtk-module

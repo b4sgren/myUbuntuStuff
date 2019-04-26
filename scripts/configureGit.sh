b095b6e9c760deda62a4e640bf0ccb71b811a1ce
Brendon Forsgren <bforsgren29@gmail.com>
2019-04-26 10:19:17 -0600

    Added two scripts to file

---
+++ scripts/configureGit.sh
@@ -0,0 +1,5 @@
+sudo apt update
+
+sudo apt install git
+git config --global user.name "Brendon Forsgren"
+git config --global user.email "bforsgren29@gmail.com"
--- scripts/pipInstall.sh
+++ scripts/pipInstall.sh
@@ -2,3 +2,6 @@ sudo apt update && sudo apt -y upgrade
 
 pip3 install --upgrade pip setuptools wheel --user
 pip2 install --upgrade pip setuptools wheel --user
+
+echo "Powering off"
+sudo poweroff

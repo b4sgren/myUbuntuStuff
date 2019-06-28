2b45b06316fbb888f0e07c6c60577d8993d3968b
Brendon Forsgren <brendon.forsgren@nearearth.aero>
2019-06-28 16:15:44 -0400

    Added settings to isolate windows in workspaces

--- scripts/primary_stuff.sh
+++ scripts/primary_stuff.sh
@@ -2,3 +2,6 @@ sudo apt install exfat-utils htop ssh mesa-utils mesa-utils-extra build-essentia
 sudo apt install libopenblas-dev liblapacke-dev tesseract-ocr libtesseract-dev libleptonica-dev libboost-all-dev
 sudo apt install libtbb-dev ocl-icd-opencl-dev ocl-icd-libopencl1 opencl-headers clinfo libffi6 libffi-dev gcc cmake
 sudo apt install pybind11-dev gdb libtool make libcanberra-gtk-module xclip
+
+gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.shell.extensions.dash-to-dock isolate-workspaces true

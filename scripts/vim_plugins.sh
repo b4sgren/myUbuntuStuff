6f5daf05b26abf45d8bf79d8ccfdb857f85c93aa
Brendon Forsgren <bforsgren29@gmail.com>
2019-09-26 10:26:54 -0600

    Added this vim plugin

---
+++ scripts/vim_plugins.sh
@@ -0,0 +1,7 @@
+# surround plugin.
+mkdir -p ~/.vim/pack/tpope/start
+mkdir -p ~/.vim/plugin
+cd ~/.vim/pack/tpope/start
+git clone https://tpope.io/vim/surround.git
+vim -u NONE -c "helptags surround/doc" -c q
+cp ~/.vim/pack/tpope/start/surround/plugin/surround.vim ~/.vim/plugin/

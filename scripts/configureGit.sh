b96a7d7968315b0958ade039c4dc60509555e6ae
Brendon Forsgren <realpepino@gmail.com>
2020-04-14 19:28:40 -0600

    Change to git config to show my update repo because im going to delete the old one

--- scripts/configureGit.sh
+++ scripts/configureGit.sh
@@ -2,4 +2,4 @@ sudo apt update
 
 sudo apt install git
 git config --global user.name "Brendon Forsgren"
-git config --global user.email "bforsgren29@gmail.com"
+git config --global user.email "realpepino@gmail.com"

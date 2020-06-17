859328a57a7f0e7a01f5e1609514abe4cc67fa25
Brendon Forsgren <realpepino@gmail.com>
2020-06-17 14:57:17 -0600

    Added global ignores to my configureGit script

--- scripts/configureGit.sh
+++ scripts/configureGit.sh
@@ -3,3 +3,10 @@ sudo apt update
 sudo apt install git
 git config --global user.name "Brendon Forsgren"
 git config --global user.email "realpepino@gmail.com"
+
+mkdir -p ~/.config/git
+touch ~/.config/git/.gitignore
+echo build >> ~/.config/git/.gitignore
+echo .vscode >> ~/.config/git.gitignore
+
+git config --global core.exculdesfile ~/.config/git/.gitignore

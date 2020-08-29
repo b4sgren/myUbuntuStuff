9a300912ccf6be3b5f51cc0f2d9907dcb5477291
Brendon Forsgren <realpepino@gmail.com>
2020-08-29 15:27:38 -0600

    Updated the scripts I have. Maybe I should add some for ceres and some others

--- scripts/installTerminator.sh
+++ scripts/installTerminator.sh
@@ -1,4 +1,9 @@
 echo_blue "Installing Terminator"
 sudo apt install -y terminator
 echo_green "Terminator installed"
-echo_red "Add coping terminator.config here!!!"
+
+echo_blue "Copying terminator Configuration"
+
+cp ../settings/terminator_config.txt ~/.config/terminator/config
+
+echo_green "Configuration copied"
--- scripts/setup_three.sh
+++ scripts/setup_three.sh
@@ -1,6 +1,7 @@
+echo_blue "Copying dotfiles into the home directory"
 cp ../files/dotfiles/.bashrc ~/
 cp ../files/dotfiles/.bashaliases ~/
 cp ../files/dotfiles/.vimrc ~/
 cp ../files/dotfiles/.rosrc ~/
 cp ../files/dotfiles/.rosaliases ~/
-cp ../settings/terminator_config.txt ~/.config/terminator/config
+echo_green "Dotfiles copied"

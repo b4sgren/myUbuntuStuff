3a9b052f4d733fac352a50c93bbe54d60e1f2c58
Brendon Forsgren <realpepino@gmail.com>
2021-09-15 17:19:20 -0600

    Auto install other plugins

--- scripts/vim_plugins.sh
+++ scripts/vim_plugins.sh
@@ -1,7 +1,8 @@
 
 echo_blue "Installin vim"
 # surround plugin.
-sudo apt install -y vim
+sudo apt purge vim
+sudo apt install -y vim-gkt
 echo_green "Vim installed"
 
 echo_blue "Installing Vim Plugins"
@@ -12,6 +13,18 @@ cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/surround.git
 vim -u NONE -c "helptags surround/doc" -c q
 cp ~/.vim/pack/tpope/start/surround/plugin/surround.vim ~/.vim/plugin/
-cd ~/myUbuntuStuff/scripts
 
+# cutlass, yoink, and subversive plugins
+mkdir -p ~/.vim/pack/svermeulen/start
+cd ~/.vim/pack/svermeulen/start
+git clone https://github.com/svermeulen/vim-cutlass.git
+git clone https://github.com/svermeulen/vim-yoink.git
+git clone https://github.com/svermeulen/vim-subversive.git
+
+#nerdtree plugin
+mkdir -p ~/.vim/pack/preservim/start
+cd ~/.vim/pack/preservim/start
+git clone https://github.com/preservim/nerdtree.git
+
+cd ~/myUbuntuStuff/scripts
 echo_green "Vim plugins Installed"
